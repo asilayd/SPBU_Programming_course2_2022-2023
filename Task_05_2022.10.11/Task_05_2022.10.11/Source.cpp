a080b9c3365a565d7f89e57b3d97774a314c0319
Extra7Virgin <danilxaritonov777@gmail.com>
2022-10-12 10:00:45 +0300

    5task

---
+++ Task_05_2022.10.11/Task_05_2022.10.11/Source.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class Vector {
+private:
+
+	int capacity;
+	int count;
+	T* data;
+
+	void expand()
+	{
+		this->capacity *= 2;
+		T* newdata = new T[capacity];
+		for (int i = 0; i < count; ++i)
+		{
+			newdata[i] = data[i];
+		}
+		delete[] this->data;
+		this->data = newdata;
+
+	}
+
+public:
+
+	Vector(int capacity = 4)
+	{
+		this->capacity = capacity;
+		this->count = 0;
+		this->data = new T[capacity];
+	}
+	Vector(const Vector& vc)
+	{
+		this->capacity = vc.capacity;
+		this->count = vc.count;
+		this->data = new T[capacity];
+		for (int i = 0; i < count; ++i)
+		{
+			this->data[i] = vc.data[i];
+		}
+	}
+	~Vector()
+	{
+		this->capacity = 0;
+		this->count = 0;
+		delete[] this->data;
+	}
+	void Add(T value)
+	{
+		if (count < capacity)
+		{
+			
+			data[count] = value; 
+			count += 1;
+		}
+		else
+		{
+			expand();
+			data[count] = value;
+			count += 1;
+		}
+	}
+	void Print()
+	{
+		for (int i = 0; i < count; ++i)
+		{
+			cout << data[i] << " ";
+		}
+		cout << endl;
+	}
+
+	T operator[](int p)
+	{
+		return this->data[p];
+	}
+	void operator=(const Vector& vc)
+	{
+		this->capacity = vc.capacity;
+		this->count = vc.count;
+		delete[] this->data;
+		T* newdata = new T[capacity];
+		for (int i = 0; i < count; ++i)
+		{
+			newdata[i] = vc.data[i];
+		}
+		this->data = newdata;
+	}
+	friend ostream& operator<<(ostream& stream, const Vector& vc)
+	{
+		stream << "[" << vc.count << "/" << vc.capacity << "] {";
+		if (vc.count == 0)
+		{
+			stream << "EMPTY }";
+		}
+		else {
+			for (int i = 0; i < vc.count - 1; ++i)
+			{
+				stream << vc.data[i] << ", ";
+			}
+			stream << vc.data[vc.count - 1] << "}" << endl;
+		}
+		
+		return stream;
+	}
+};
+
+int main()
+{
+	
+	Vector<int> vc(10);
+	for (int i = 0; i < 14; ++i)
+	{
+		vc.Add(i);
+		
+		
+	}
+	cout << vc << endl;
+	for (int i = 0; i < 14; ++i)
+	{
+		cout << vc[i] << endl;
+
+
+	}
+	
+}
